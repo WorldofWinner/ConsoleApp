05ac85f846f4174e36066ae16e11ed2bfe18d71e
Viktoria Typusiak <v.typusiak@sp231w.onmicrosoft.com>
2023-06-02 13:08:57 +0200

    Homework day 4

--- projekt21/projekt21/Program.cs
+++ projekt21/projekt21/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("how are you?");
+﻿string name = "Ruslan";
+var age = 30;
+bool isWoman = false;
+if (isWoman && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+if ( isWoman == false && age < 18)
+{
+    Console.WriteLine("Niepewniletni Man");
+}
+if (name == "Ruslan" && isWoman == false && age ==30 )
+{
+    Console.WriteLine("Yas I did it");
+}
+
+
+// kobieta ponizej 30 lat
+// Ewa ponizej 33 lat
+// Niepewnoletni Man
